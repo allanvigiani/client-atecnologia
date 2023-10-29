d8d4cff08a4f1fa67bd00c8dc66cfe961f889c99
Allan <allan.vigiani@smartnx.com>
2023-10-29 16:32:38 -0300

    Rota GET para company

--- backend/api/auth/auth.js
+++ backend/api/auth/auth.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
 
 import auth from './routers/auth.js';
+import * as swaggerJsonDocs from './swagger.json';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('auth-api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsonDocs));
 
 app.use('/auth', auth);
 
