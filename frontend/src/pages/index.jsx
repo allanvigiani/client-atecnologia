4b924e56ddfb02c1ae58dbf8ebbb73ba29e4f563
Allan <allan.vigiani@smartnx.com>
2023-11-16 20:29:26 -0300

    feat: Inserindo no docker-compose.yml a incialização do frontend

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,8 +1,3 @@
-import Login from "./login/index";
-import Register from "./register/index";
-import Company from "./company/index";
-import Schedule from "./schedule/index";
-import Index from "./"
 import Users from "./users/[userDynamic]";
 
 export default function Home() {
