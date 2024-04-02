0ba94a4fb0559ef910bfe3b07265f10ad76b7dab
Allan <allan.vigiani@smartnx.com>
2024-04-01 21:29:37 -0300

    feat: Adicionando tabela de usuário e sessão do usuário.

--- migrations/202310111130.sql
+++ migrations/202310111130.sql
@@ -29,6 +29,35 @@ CREATE TABLE IF NOT EXISTS "company_sessions" (
     CONSTRAINT "company_sessions_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "company" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "address" VARCHAR(250),
+    "contact_phone" VARCHAR(20),
+    "login_attempts" INT DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX IF NOT EXISTS "user_id_key" ON "user"("id");
+CREATE UNIQUE INDEX IF NOT EXISTS "user_email_key" ON "user"("email");
+
+CREATE TABLE IF NOT EXISTS "user_sessions" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "start_login" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "end_login" TIMESTAMP(3),
+    "token" TEXT,
+    CONSTRAINT "user_sessions_pkey" PRIMARY KEY ("id")
+);
+
+ALTER TABLE "user_sessions" ADD CONSTRAINT "user_sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
 -- CreateTable: services
 CREATE TABLE IF NOT EXISTS "services" (
     "id" SERIAL NOT NULL,
