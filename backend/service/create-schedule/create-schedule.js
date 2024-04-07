3948090535ed32a6d130b570ffc0c46234b62ec7
Allan <allan.vigiani@smartnx.com>
2024-04-07 20:15:56 -0300

    .

--- backend/service/create-schedule/create-schedule.js
+++ backend/service/create-schedule/create-schedule.js
@@ -1,6 +1,9 @@
 import connectRabbitMq from './connections/queue-connection.js';
 import dotenv from 'dotenv';
 import ScheduleRepository from './repositories/schedule-repository.js';
+
+const scheduleRepository = new ScheduleRepository();
+
 import express from 'express';
 
 const app = express();
@@ -15,17 +18,20 @@ async function createSchedule() {
 
     try {
 
-        const scheduleRepository = new ScheduleRepository();
         const channel = await connectRabbitMq();
 
         const data = consumeQueue('user/schedule_information');
 
-        const { company_id, user_id, service_id, service_hour_id, service_day_id, created_at } = data;
+        const { company_id, user_id, service_id, service_hour_id, service_day_id } = data;
+
+        if (!company_id || !user_id || !service_id || !service_hour_id || !service_day_id) {
+            return { message: `Faltando parâmetros do agendamento do serviço!`};
+        }
 
         const result = await scheduleRepository.createSchedule(data);
         if (!result) {
             const errorMessage = `Erro ao agendar o serviço. Tente novamente mais tarde!`;
-            return { message: errorMessage, status: 500 };
+            return { message: errorMessage};
         }
 
         const company = await scheduleRepository.getCompany(company_id);
@@ -50,7 +56,7 @@ async function createSchedule() {
         await channel.sendToQueue('client/send_email', Buffer.from(message));
         console.log(" [x] Enviado '%s'", message);
 
-        return { message: `Serviço agendado com sucesso!`, status: 201 };
+        return { message: `Serviço agendado com sucesso!`};
     } catch (error) {
         return { message: error.message, status: 500 };
     } finally {
