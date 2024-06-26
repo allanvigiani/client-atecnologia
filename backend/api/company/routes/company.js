b8599222df9e5dc17fc8fefe319d02063131edb1
Allan <allan.vigiani@smartnx.com>
2024-06-26 10:09:20 -0300

    fix: Resolvendo problema de cadastro/lentidão no cadastro de empresa.

--- backend/api/company/routes/company.js
+++ backend/api/company/routes/company.js
@@ -11,6 +11,7 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
     const result = await companyController.createCompany(req.body);
+    await companyController.createCompany(req.body); // Resolve problema de lentidão de cadastro na Vercel
     res.status(result.status).json({ message: result.message });
 });
 
